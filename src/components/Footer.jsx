52aabe5ce1eab2967ad098d9aeeb3f1b12215119
Flávio Mota <flavio.belizario.mota@gmail.com>
2026-05-13 09:19:45 -0300

    feat: criando o repositorio da disciplina

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Desenvolvido por Flávio Mota — Programação Frontend — UNIVÁS</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Pokédex</h1>
+      <p>Sua enciclopédia Pokémon</p>
+    </header>
+  )
+}
+
+export default Header
